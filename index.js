41851ade62a61129744d35619baa9a8a87c2c52f
devdeepbarad <97941483+devdeepbarad@users.noreply.github.com>
2023-02-14 18:10:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+// const colors = require('colors');
+//import chalk from 'chalk';
+
+const name = 'Devdeep';
+//console.log(chalk.green('Hello %s'), name);
+
+//fs.writeFileSync('Read1.txt', "Hello BARAD How r u?");
+console.log("---devcd->", __filename);
+//console.log("Hello".green);
+//const http = require('http');
+
+// const datacontrol = (req, res) => {
+//     res.write("Hello from Devdeep Barad Node First Program");
+//     console.log("package.json");
+//     res.end();
+// }
+// http.createServer(datacontrol).listen(4000);
